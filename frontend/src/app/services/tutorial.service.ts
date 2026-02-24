43cc5ab4bbb4a16391ca2ee957a37bbc61ddf5e3
Ayush <aws.ayush039@gmail.com>
2026-02-24 20:19:58 +0530

    tutorial.service.ts

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://http://52.66.251.225:8080/api/tutorials';
+const baseUrl = 'http://52.66.251.225:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
